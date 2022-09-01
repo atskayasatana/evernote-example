3b2b5bd597383d578ebf5c5d0dfa57e0a3eed118
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-09-02 00:15:45 +0300

    Update dump_inbox.py

--- dump_inbox.py
+++ dump_inbox.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 import argparse
 
 from evernote.api.client import EvernoteClient
 from evernote.api.client import NoteStore
 from bs4 import BeautifulSoup
-
 from config import Settings
 
-    
+
 def get_notebook_list(note_store, notebook_guid, number=10, offset=0):
     _filter = NoteStore.NoteFilter(notebookGuid=notebook_guid)
     resultSpec = NoteStore.NotesMetadataResultSpec(
@@ -25,11 +24,12 @@ def get_notebook_list(note_store, notebook_guid, number=10, offset=0):
     )
 
     # this determines which info you'll get for each note
-    return note_store.findNotesMetadata(_filter, offset, number, resultSpec);
+    return note_store.findNotesMetadata(_filter, offset, number, resultSpec)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=u'Dumps notes from Evernote inbox to console')
+    parser = argparse.ArgumentParser(description="Dumps notes from Evernote\
+                                                  inbox to console")
     parser.add_argument('number',
                         nargs='?',
                         type=int,
@@ -41,16 +41,17 @@ if __name__ == '__main__':
 
     client = EvernoteClient(
         token=config.EVERNOTE_PERSONAL_TOKEN,
-        sandbox=False
+        sandbox=True
     )
     note_store = client.get_note_store()
 
-    notes = get_notebook_list(note_store, config.INBOX_NOTEBOOK_GUID, args.number).notes
+    notes = get_notebook_list(note_store,
+                              config.INBOX_NOTEBOOK_GUID,
+                              args.number).notes
 
-    # print('Notes', notes)
-    
     for counter, note in enumerate(notes, start=1):
-        print('\n--------- %s ---------' % counter)
-        content = note_store.getNoteContent(note.guid)  # kwargs will be skipped by api because of bug
+        print(('\n--------- %s ---------' % counter))
+        # kwargs will be skipped by api because of bug
+        content = note_store.getNoteContent(note.guid)
         soup = BeautifulSoup(content, "html.parser")
-        print(soup.get_text())
+        print((soup.get_text()))
