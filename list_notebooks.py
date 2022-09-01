8eaaf28ed89089b968f6ca1c3e2ea3e70736ebab
Anna <88509622+atskayasatana@users.noreply.github.com>
2022-09-02 00:16:16 +0300

    Update list_notebooks.py

--- list_notebooks.py
+++ list_notebooks.py
@@ -1,17 +1,18 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
+
 from evernote.api.client import EvernoteClient
 
 from config import Settings
 
-    
+
 if __name__ == '__main__':
     config = Settings()
     client = EvernoteClient(
         token=config.EVERNOTE_PERSONAL_TOKEN,
-        sandbox=False
+        sandbox=True
     )
     note_store = client.get_note_store()
 
     notebooks = note_store.listNotebooks()
     for notebook in notebooks:
-        print('%s - %s' % (notebook.guid, notebook.name))
+        print(('%s - %s' % (notebook.guid, notebook.name.decode('utf8'))))
